bd2705b8ca5d9425d386c2153e2bbdd0e0e6347c
Ahmed Elakeed <ahmed.elaked20@gmail.com>
2023-10-29 22:13:36 -0500

    department component, service and DTO

--- src/app/component/department/department.component.ts
+++ src/app/component/department/department.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {DepartmentService} from "../../service/department.service";
+import {DepartmentDTO} from "../../dto/DepartmentDTO";
 
 @Component({
   selector: 'app-department',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./department.component.css']
 })
 export class DepartmentComponent {
+  public departmentsDTOs:DepartmentDTO[]=[];
+  constructor(private departmentService:DepartmentService) {
+  }
 
+  ngOnInit(): void {
+    this.getAllDepartments();
+  }
+
+  public getAllDepartments(){
+    this.departmentService.fetchAllDepartments().subscribe(
+      (response)=>{
+        console.log(response);
+        this.departmentsDTOs=response.data;
+      }
+    )
+  }
 }
--- src/app/service/department.service.ts
+++ src/app/service/department.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {ApiGenericResopnse} from "../dto/ApiGenericResopnse";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentService {
 
-  constructor() { }
+  private DEPARTMENT_URL= environment.BASE_URL + 'departments'
+  constructor(private http:HttpClient) { }
+
+  public fetchAllDepartments():Observable<ApiGenericResopnse>{
+    return this.http.get<ApiGenericResopnse>(this.DEPARTMENT_URL);
+  }
 }
